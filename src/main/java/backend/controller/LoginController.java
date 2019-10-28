93fde619cfa608177b7a2e9a04ada0b812249350
chungwwei <chungwwei223@gmail.com>
2019-10-28 17:09:15 -0400

    redirect back to login

--- src/main/java/backend/controller/LoginController.java
+++ src/main/java/backend/controller/LoginController.java
@@ -1,33 +1,55 @@
 package backend.controller;
 
+import backend.dto.UserDTO;
+import backend.model.User;
+import backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class LoginController {
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage(@RequestParam(value = "error", required = false) String error,
-                            @RequestParam(value = "logout", required = false) String logout,
-                            Model model) {
 
-        String errorMessge = null;
-        if(error != null) {
-            errorMessge = "Username or Password is incorrect !!";
-        }
-        if(logout != null) {
-            errorMessge = "You have been successfully logged out !!";
-        }
-        model.addAttribute("errorMessge", errorMessge);
-        return "login";
+    @Autowired
+    UserService userService;
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ResponseEntity loginPage(@RequestBody UserDTO userDTO) {
+
+        System.out.println("Login Controller is called");
+
+        User existing = userService.findByEmail(userDTO.getEmail_address());
+        if (existing == null)
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+
+        if (existing.getPassword() != userDTO.getPassword())
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+
+
+
+
+
+//        String errorMessge = null;
+//        if(error != null) {
+//            errorMessge = "Username or Password is incorrect !!";
+//        }
+//        if(logout != null) {
+//            errorMessge = "You have been successfully logged out !!";
+//        }
+//        model.addAttribute("errorMessge", errorMessge);
+//        return "login";
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @RequestMapping(value="/logout", method = RequestMethod.GET)
