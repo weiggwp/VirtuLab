5485d928cb6201efc2ecca0df254620b5fcd1f75
Ying Zhang <yingz1985@yahoo.com>
2019-10-28 22:53:31 -0400

    started student do_lab page - commiting to merge w master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from './components/login.jsx';
 import SignUp from './components/signup.jsx';
 import Account from './components/account_settings.jsx';
 import StudentHome from './components/student_home.jsx';
+import DoLab from './components/student_lab.jsx';
 
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -22,6 +23,7 @@ class App extends Component {
               <Route exact path="/signup" component={SignUp} />
               <Route exact path="/account_settings" component={Account} />
               <Route exact path="/student_home" component={StudentHome} />
+              <Route exact path="/do_lab" component={DoLab} />
 
             </Switch>
           </div>
