ddce975a3f32b557cc1b214d0a47be5551b251b7
erubinovitz <erubinovitz@yahoo.com>
2019-12-15 22:45:09 -0500

    timezone changes

--- frontend/src/components/Statistics.jsx
+++ frontend/src/components/Statistics.jsx
@@ -246,8 +246,9 @@ class Statistics extends Component {
         let stepData=this.state.stepData;
 
         let stepOutput=[];
-        if (stepData[this.state.currentStepIndex].max==0){
+        if (stepData[this.state.currentStepIndex]==undefined||stepData[this.state.currentStepIndex].max==0){
             stepOutput = ["No students have completed this step."]
+            this.state.stepData[this.state.currentStepIndex].min=0
         }
         else{
             stepOutput[0]="Average number of tries: "+ this.state.stepData[this.state.currentStepIndex].avg
